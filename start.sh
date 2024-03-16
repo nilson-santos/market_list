1b15ab6c52cd1a3af34928574f38af4c6d0dbb29
Nilson Santos <evanilson.santos@luizalabs.com>
2024-03-16 13:55:49 -0300

    Settings for containers

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst '$DJANGO_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
