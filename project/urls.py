c392f5477e2743087b13390253f16a946e241303
Nilson Santos <evanilson.santos@luizalabs.com>
2024-03-17 17:28:22 -0300

    adjusting deploy

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from .views import django_health_check
 
@@ -21,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('grocery_list.urls')),
     path('django-health-check/', django_health_check)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
