143fc2f002cdfa9f1727f04832389994d5d85d28
Nilson Santos <evanilson.santos@luizalabs.com>
2024-03-17 15:29:46 -0300

    adjusting deploy

--- project/settings.py
+++ project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     'static',
