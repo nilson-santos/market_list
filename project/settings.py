a7de8107eeb34a8d7eb5df9e15ed0b2e673d15e6
Nilson Santos <evanilson.santos@luizalabs.com>
2024-03-18 14:44:40 -0300

    adjusting deploy

--- project/settings.py
+++ project/settings.py
@@ -69,7 +69,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'project.context_processors.cfg_assets_root',
             ],
         },
     },
